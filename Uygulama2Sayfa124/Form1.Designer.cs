e02a7d3b80cbb8512fd9f64276eb92783f4fe0e2
muttalip591 <muttalip591@BPC13>
2024-01-09 09:51:38 +0300

    Proje dosyası ekle.

---
+++ Uygulama2Sayfa124/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Uygulama2Sayfa124
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lbListele = new System.Windows.Forms.ListBox();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(36, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad ";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdSoyad.Location = new System.Drawing.Point(171, 88);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 31);
+            this.txtAdSoyad.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(304, 88);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(122, 34);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lbListele
+            // 
+            this.lbListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbListele.FormattingEnabled = true;
+            this.lbListele.ItemHeight = 25;
+            this.lbListele.Location = new System.Drawing.Point(443, 88);
+            this.lbListele.Name = "lbListele";
+            this.lbListele.Size = new System.Drawing.Size(137, 179);
+            this.lbListele.TabIndex = 3;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListele.Location = new System.Drawing.Point(618, 147);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(102, 80);
+            this.btnListele.TabIndex = 4;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.lbListele);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lbListele;
+        private System.Windows.Forms.Button btnListele;
+    }
+}
+
---
+++ Uygulama2Sayfa124/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama2Sayfa124
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] isimler=new string[5];
+        int index = 0;
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            isimler[index] =txtAdSoyad.Text;
+            index++;   
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                lbListele.Items.Add(isimler[i]);
+            }
+        }
+    }
+}
